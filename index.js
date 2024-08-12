3597724ad434b3d82cfaf0c8574eb4db9604d4e8
mellambias <mellambias@gmail.com>
2024-08-12 18:39:22 +0200

    server port update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const express = require("express");
 const app = Express();
 
-const PORT = node.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 let data = [
 	{
